b2489afdf1ff34f7927cd7467254127223bfc19c
hcropsu <roopesanterisalminen@gmail.com>
2024-09-06 12:40:19 +0300

    ready for initial render deployment

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
 const morgan = require('morgan')
+const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('reqpostcontent', function(req, res) {
@@ -127,7 +129,7 @@ app.post('/api/persons', (req, res) => {
   res.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`)
 })
