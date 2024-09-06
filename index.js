97a139f1118cbd902da96adf2ed2eae0bb202e4f
hcropsu <roopesanterisalminen@gmail.com>
2024-09-06 09:46:36 +0300

    puhelinluettelon backend step6 DONE

---
+++ index.js
@@ -0,0 +1,115 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": "1"
+  },
+  { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": "2"
+  },
+  { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": "3"
+  },
+  { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": "4"
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+{/* It seems chrome isn't including the 'Date' header field in the request.
+  It also seems that it is added so late to the response that accessing
+  the 'Date' header of the response is tricky. So the date is constructed
+  artificially. */}
+
+app.get('/info', (req, res) => {
+  const numberOfPeople = persons.length
+  const numberOfPeopleString = String(numberOfPeople)
+  const dateNow = new Date().toString()
+  
+  if (numberOfPeople === 1) {
+    return (
+      res.send(
+        `Phonebook has contact info for ${numberOfPeopleString} person. <br>${dateNow}`
+      )
+    )
+  }
+  res.send(
+    `Phonebook has contact info for ${numberOfPeopleString} people. <br>${dateNow}`
+  )
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = persons.find(p => p.id === id)
+  if(person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
+const generateRandomId = () => {
+  return Math.floor(Math.random() * 1000)
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  const newName = body.name
+  const newNumber = body.number
+
+  if (!newName) {
+    return res.status(400).json({
+      error: "missing name"
+    })
+  }
+
+  if (!newNumber) {
+    return res.status(400).json({
+      error: "missing number"
+    })
+  }
+
+  if (persons.find(p => p.name === newName)) {
+    return res.status(400).json({
+      error: "name must not already exist in phonebook"
+    })
+}
+
+  const newId = generateRandomId()
+  const newPerson = {
+    name: newName,
+    number: newNumber,
+    id: newId
+  }
+  persons = persons.concat(newPerson)
+  res.json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`)
+})
