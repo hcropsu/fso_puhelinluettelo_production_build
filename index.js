cf40c9e601ffffae2154769c8341f05fc484a499
hcropsu <roopesanterisalminen@gmail.com>
2024-09-06 10:56:19 +0300

    puhelinluettelon backend step8 DONE

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+morgan.token('reqpostcontent', function(req, res) {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body)  
+  }
+})
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.reqpostcontent(req, res),
+  ].join(' ')
+}))
 
 let persons = [
   { 
